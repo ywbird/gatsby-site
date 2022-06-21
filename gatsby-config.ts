e68d2bb4d64913af72b761196932b2c66f9a4d71
ywbird <ywbird@naver.com>
2022-06-21 19:28:59 +0900

    Revert "Finished Gatsby Tutorial Part 7"
    This reverts commit d468f922cc5459a8d902ba040ad66a146d941fcc.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,7 +22,6 @@ const config: GatsbyConfig = {
       },
     },
     "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
   ],
 };
 
--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 
 interface DataType {
@@ -9,44 +8,15 @@ interface DataType {
     frontmatter: {
       title: string;
       date: string;
-      hero_image: {
-        childImageSharp: {
-          gatsbyImageData: {
-            backgroundColor?: string;
-            images: {
-              fallback: {
-                src: string;
-                srcSet: string;
-                sizes: string;
-              };
-              sources: { srcSet: string; sizes: string; type: string }[];
-            };
-            layout: "fixed" | "fullWidth" | "constrained";
-            width: number;
-            height: number;
-          };
-        };
-      };
-      hero_image_alt: string;
-      hero_image_credit_link: string;
-      hero_image_credit_text: string;
     };
     body: string;
   };
 }
 
 const BlogPost = ({ data }: { data: DataType }) => {
-  const image = data.mdx.frontmatter.hero_image.childImageSharp.gatsbyImageData;
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
-      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
-      <p>
-        Photo Credit:{" "}
-        <a href={data.mdx.frontmatter.hero_image_credit_link}>
-          {data.mdx.frontmatter.hero_image_credit_text}
-        </a>
-      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -55,19 +25,11 @@ const BlogPost = ({ data }: { data: DataType }) => {
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
-      body
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
-        hero_image_alt
-        hero_image_credit_link
-        hero_image_credit_text
-        hero_image {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
+        date(formatString: "MMMM D, YYYY")
       }
+      body
     }
   }
 `;
